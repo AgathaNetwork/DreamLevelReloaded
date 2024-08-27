9f72ac6b2b123bd47f4648cbc9bd38d98c427161
MikeWu597 <2792995083@qq.com>
2024-08-27 23:41:21 +0800

    Creation

---
+++ src/main/java/cn/kylin/dreamlevel/dreamlevelreloaded/DreamLevelReloaded.java
@@ -0,0 +1,17 @@
+package cn.kylin.dreamlevel.dreamlevelreloaded;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DreamLevelReloaded extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
